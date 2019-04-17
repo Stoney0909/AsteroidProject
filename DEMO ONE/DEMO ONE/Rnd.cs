3ccde042b077264b8526d3626c3af30ee3a74b85
SamStone09 <samuel.stone@snhu.edu>
2019-04-17 13:42:19 -0400

    Create Rnd.cs

---
+++ DEMO ONE/DEMO ONE/Rnd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEMO_ONe.Content
+{
+    class Rnd
+    {
+        Random rnds = new Random();
+
+        public Rnd()
+        {
+
+        }
+
+        int rnd(int min, int max)
+        {
+            return rnds.Next(min, max);
+        }
+    }
+}
