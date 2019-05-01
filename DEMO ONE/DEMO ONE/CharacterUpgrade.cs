b4c884d00ddb4f42522c5497ac2b06d691253a30
SamStone09 <samuel.stone@snhu.edu>
2019-05-01 02:21:34 -0400

    bruh

--- DEMO ONE/DEMO ONE/CharacterUpgrade.cs
+++ DEMO ONE/DEMO ONE/CharacterUpgrade.cs
@@ -12,80 +12,53 @@ namespace DEMO_ONe
 {
     public partial class CharacterUpgrade : Form
     {
-        public float health;
-        public int coolDown;
-        public double money;
+        public float health = 0;
+        public int coolDown = 0;
+        public double money = 0;
         public CharacterUpgrade(float health, int coolDown, double money)
         {
             this.health = health;
             this.coolDown = coolDown;
             this.money = money;
-            Updateboxes();
+
             InitializeComponent();
         }
 
         private void addhealth_Click(object sender, EventArgs e)
         {
-            if (money >= 20)
+            if (money >= 300)
             {
                 health++;
-                minusHealth.Show();
+
                 money -= 20;
             }
-            Updateboxes();
+            else
+            {
+                error.Show();
+            }
+
         }
 
         private void addcoolDown_Click(object sender, EventArgs e)
         {
-            if (money >= 20)
-            {
-                coolDown -= 10;
-                minusAmountofPro.Show();
-                money -= 20;
-            }
-            Updateboxes();
-        }
-
-        private void minusHealth_Click(object sender, EventArgs e)
-        {
-            health -= 1;
-            money += 10;
-            if (health == 1)
-            {
-                minusHealth.Hide();
-            }
-            Updateboxes();
-        }
-
-        private void minusAmountofPro_Click(object sender, EventArgs e)
-        {
-            if (money >= 20)
+            if (money >= 300)
             {
                 coolDown -= 10;
                 addcoolDown.Show();
                 money -= 20;
-                Updateboxes();
+            }
+            else
+            {
+                error.Show();
             }
         }
+
+
 
         private void continuebutton_Click(object sender, EventArgs e)
         {
             this.Close();
         }
-        private void healthbox_TextChanged(object sender, EventArgs e)
-        {
 
-        }
-        private void Updateboxes()
-        {
-            HealthTextBox.Text = " ";
-            //ProTextBox.Text = coolDown.ToString();
-            //totalmoney.Text = money.ToString();
-        }
-
-        private void CharacterUpgrade_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
